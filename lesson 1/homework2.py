100443cd02e6b76486aeb559a7fc0ef2d8476f1d
Roman <123@mail.ru>
2021-04-03 11:11:32 +0300

    переделал задания 2 и 3

--- lesson 1/homework2.py
+++ lesson 1/homework2.py
@@ -3,5 +3,5 @@ var_int=int(input('enter you count seconds:'))
 var_hour=var_int//3600
 var_min=(var_int-var_hour*3600)//60
 var_sec=var_int-var_hour*3600-var_min*60
-print('Это соответствует (чч:мм:сс):{}:{}:{}'.format(var_hour,var_min,var_sec))
+print('Это соответствует (чч:мм:сс):{:02}:{:02}:{:02}'.format(var_hour,var_min,var_sec))
 
--- lesson 1/homework3.py
+++ lesson 1/homework3.py
@@ -1,6 +1,13 @@
 #задание 3
-
+'''
 var_int=int(input('enter you integer number:'))
 var1=var_int*10+var_int
 var2=var_int*100+var1
+#n+nn+nn 3+33+33=...   10+1010+101010=...
 print('{}+{}+{}={}'.format(var_int,var1,var2,var_int+var1+var2))
+'''
+# 2
+var_str=input('enter you integer number:')
+var1=var_str+var_str
+var2=var1+var_str
+print('{}+{}+{}={}'.format(var_str,var1,var2,int(var_str)+int(var1)+int(var2)))
